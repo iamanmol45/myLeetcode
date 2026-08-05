cb110d0a7ddd2fb5fff25166d00fd66acde59813
Anmol Jana <ANMOL.JANA54@nmims.in>
2026-08-05 23:18:23 +0530

    Time: 0 ms (100%), Space: 10.8 MB (41.87%) - LeetHub

---
+++ 1732-find-the-highest-altitude/1732-find-the-highest-altitude.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int largestAltitude(vector<int>& gain) {
+        int altitude=0;
+        int highest=0;
+        for(int g:gain){
+            altitude+=g;
+            highest=max(highest,altitude);
+        }
+        return highest;
+    }
+};
